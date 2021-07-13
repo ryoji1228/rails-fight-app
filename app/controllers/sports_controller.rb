ca9433d3366e6ca94336701d69166d9ff2b07a81
EC2 Default User <ec2-user@ip-172-31-25-130.ap-northeast-1.compute.internal>
2021-07-13 06:01:57 +0000

    controller変更

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,5 +1,7 @@
 class SportsController < ApplicationController
     
+    before_action :set_sport, only: [:show, :edit]
+    
     def index
         p current_user
         @sports = current_user.sports
@@ -7,7 +9,6 @@ class SportsController < ApplicationController
     end
     
     def show
-        @sport = Sport.where(user_id: current_user.id).find(params[:id])
     end
     
     def new
@@ -15,9 +16,9 @@ class SportsController < ApplicationController
     end
     
     def create
-        sport_params = params.require(:sport).permit(:year, :month, :name1, :name2, :result)
-        sport_params[:user_id] = current_user.id
         @sport = Sport.new(sport_params)
+        @sport[:user_id] = current_user.id
+        p @sport
         if @sport.save
             flash[:notice] = "データを1件登録しました"
             redirect_to sports_path
@@ -28,12 +29,10 @@ class SportsController < ApplicationController
     end
     
     def edit
-        @sport = Sport.where(user_id: current_user.id).find(params[:id])
     end
     
     def update
         @sport = Sport.find(params[:id])
-        sport_params = params.require(:sport).permit(:year, :month, :name1, :name2, :result)
         if @sport.update(sport_params)
             flash[:notice] = "データを1件更新しました"
             redirect_to sports_path
@@ -44,10 +43,21 @@ class SportsController < ApplicationController
     end
     
     def destroy
-        @sport = current_user.sports
+        @sport = current_user.sports.find(params[:id])
         @sport.destroy
         flash[:notice] = "削除しました"
         redirect_to sports_path
     end
+    
+    private
+    
+    def set_sport
+        @sport = Sport.where(user_id: current_user.id).find(params[:id])
+    end
+    
+    def sport_params
+        params.require(:sport).permit(:year, :month, :name1, :name2, :result)
+    end
+    
     
 end
